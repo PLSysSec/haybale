9a17c3df4af9419d85c415f3d8b809da0f346b92
Craig Disselkoen <craigdissel@gmail.com>
2019-06-18 15:59:33 -0700

    switch to inkwell

---
+++ c_examples/basic/basic.ll
@@ -0,0 +1,44 @@
+; ModuleID = 'basic.c'
+source_filename = "basic.c"
+target datalayout = "e-m:o-i64:64-f80:128-n8:16:32:64-S128"
+target triple = "x86_64-apple-macosx10.14.0"
+
+; Function Attrs: noinline nounwind optnone ssp uwtable
+define i32 @basic(i32, i32) #0 {
+  %3 = alloca i32, align 4
+  %4 = alloca i32, align 4
+  %5 = alloca i32, align 4
+  store i32 %0, i32* %4, align 4
+  store i32 %1, i32* %5, align 4
+  %6 = load i32, i32* %4, align 4
+  %7 = load i32, i32* %5, align 4
+  %8 = icmp slt i32 %6, %7
+  br i1 %8, label %9, label %13
+
+; <label>:9:                                      ; preds = %2
+  %10 = load i32, i32* %4, align 4
+  %11 = load i32, i32* %5, align 4
+  %12 = sub nsw i32 %10, %11
+  store i32 %12, i32* %3, align 4
+  br label %17
+
+; <label>:13:                                     ; preds = %2
+  %14 = load i32, i32* %4, align 4
+  %15 = load i32, i32* %5, align 4
+  %16 = mul nsw i32 %14, %15
+  store i32 %16, i32* %3, align 4
+  br label %17
+
+; <label>:17:                                     ; preds = %13, %9
+  %18 = load i32, i32* %3, align 4
+  ret i32 %18
+}
+
+attributes #0 = { noinline nounwind optnone ssp uwtable "correctly-rounded-divide-sqrt-fp-math"="false" "disable-tail-calls"="false" "less-precise-fpmad"="false" "no-frame-pointer-elim"="true" "no-frame-pointer-elim-non-leaf" "no-infs-fp-math"="false" "no-jump-tables"="false" "no-nans-fp-math"="false" "no-signed-zeros-fp-math"="false" "no-trapping-math"="false" "stack-protector-buffer-size"="8" "target-cpu"="penryn" "target-features"="+cx16,+fxsr,+mmx,+sahf,+sse,+sse2,+sse3,+sse4.1,+ssse3,+x87" "unsafe-fp-math"="false" "use-soft-float"="false" }
+
+!llvm.module.flags = !{!0, !1}
+!llvm.ident = !{!2}
+
+!0 = !{i32 1, !"wchar_size", i32 4}
+!1 = !{i32 7, !"PIC Level", i32 2}
+!2 = !{!"clang version 7.0.1 (tags/RELEASE_701/final)"}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,17 @@
-use llvm_sys::bit_reader::LLVMGetBitcodeModule2;
-use llvm_sys::core::LLVMCreateMemoryBufferWithContentsOfFile;
-use llvm_sys::core::LLVMGetFirstFunction;
-use llvm_sys::prelude::*;
-use std::ptr;
-use libc::c_char;
-use std::ffi::{CString, CStr};
+use inkwell::module::Module;
+use std::path::Path;
 
 fn main() {
-    let filepath_cstring = CString::new("c_examples/basic/basic.bc").unwrap();
-    unsafe {
-        let filepath: *const c_char = filepath_cstring.as_ptr();
-        let mut out_message: *mut c_char = ptr::null_mut() as *mut c_char;
-        let mut mem_buf: LLVMMemoryBufferRef = ptr::null_mut();
-        let status = LLVMCreateMemoryBufferWithContentsOfFile(filepath, &mut mem_buf, &mut out_message);
-        if status != 0 {
-            if let Ok(message) = CStr::from_ptr(out_message).to_str() {
-                panic!("LLVMCreateMemoryBufferWithContentsOfFile failed with code {}: {}", status, message);
-            } else {
-                panic!("LLVMCreateMemoryBufferWithContentsOfFile failed with code {}, failed to recover error message", status);
-            }
-        }
-        let mut llvm_mod: LLVMModuleRef = ptr::null_mut();
-        let status = LLVMGetBitcodeModule2(mem_buf, &mut llvm_mod);
-        if status != 0 {
-            panic!("LLVMGetBitcodeModule2 failed");
-        }
-        let func = LLVMGetFirstFunction(llvm_mod);
-        println!("First function in the LLVM file is {:?}", func)
+    let filepath = Path::new("/Users/craig/pitchfork-rs/c_examples/basic/basic.bc");
+    if filepath.exists() {
+        println!("Path definitely exists");
+    } else {
+        panic!("Specified path does not exist");
+    }
+    let llvm_mod = Module::parse_bitcode_from_path(&filepath).expect("Failed to parse module");
+    if let Some(func) = llvm_mod.get_first_function() {
+        println!("First function in the LLVM file is named {:?}", func.get_name());
+    } else {
+        println!("No functions found in the LLVM file");
     }
 }
