108aafb7cc918d9d06212200a63b14aae562b8a6
Craig Disselkoen <craigdissel@gmail.com>
2019-07-01 22:40:03 -0700

    snazzier main with more argument options

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,12 +5,24 @@ use std::path::Path;
 
 fn main() {
     env_logger::init();
+    // With 0 args, finds zeroes of all the functions in "basic"
+    // With 1 arg (a function name), finds zero of that function in "basic"
+    // With 2 args (file then function), finds zero of that function in that file
     let firstarg = std::env::args().nth(1);
-    let filepath = Path::new("c_examples/basic/basic.bc");
-    let llvm_mod = Module::parse_bitcode_from_path(&filepath).expect("Failed to parse module");
+    let secondarg = std::env::args().nth(2);
+    let (modname, funcname): (String, Option<String>) = if let Some(funcname) = secondarg {
+        (firstarg.expect("Have second arg, so we must have first arg"), Some(funcname))
+    } else if let Some(funcname) = firstarg {
+        ("basic".to_owned(), Some(funcname))
+    } else {
+        ("basic".to_owned(), None)
+    };
+    let pathstring: String = format!("c_examples/{}/{}.bc", modname, modname);
+    let filepath = Path::new(&pathstring);
+    let llvm_mod = Module::parse_bitcode_from_path(&filepath).unwrap_or_else(|e| panic!("Failed to parse module at path {}: {}", filepath.display(), e));
     let functions: Box<Iterator<Item = FunctionValue>>;
-    if let Some(funcname) = firstarg {
-        functions = Box::new(std::iter::once(llvm_mod.get_function(&funcname).unwrap_or_else(|| panic!("Failed to find function named {}", funcname))));
+    if let Some(f) = funcname {
+        functions = Box::new(std::iter::once(llvm_mod.get_function(&f).unwrap_or_else(|| panic!("Failed to find function named {}", f))));
     } else {
         functions = Box::new(FunctionIterator::new(&llvm_mod));
     }
