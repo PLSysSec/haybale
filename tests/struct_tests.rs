7d1960a2365350767c2ba296b8fc500224e61a15
Craig Disselkoen <craigdissel@gmail.com>
2019-07-16 20:38:51 -0700

    wrapping arithmetic in struct tests

--- tests/struct_tests.rs
+++ tests/struct_tests.rs
@@ -1,6 +1,7 @@
 use llvm_ir::*;
 use pitchfork_rs::*;
 use std::path::Path;
+use std::num::Wrapping;
 
 fn init_logging() {
     // capture log messages with test harness
@@ -49,12 +50,14 @@ fn two_ints_both() {
     let func = module.get_func_by_name("two_ints_both").expect("Failed to find function");
     let args = find_zero_of_func(func, 20).expect("Failed to find zero of function");
     assert_eq!(args.len(), 1);
-    let x = args[0].unwrap_to_i32();
-    let _tiel1 = x + 2;
-    let tiel2 = x + 3;
-    let tiel1 = tiel2 - 10;
-    let tiel2 = tiel1 + 7;
-    assert_eq!(tiel2 - 3, 0);
+    let x = Wrapping(args[0].unwrap_to_i32());
+    println!("x = {}", x);
+    let _tiel1 = x + Wrapping(2);
+    let tiel2 = x + Wrapping(3);
+    let tiel1 = tiel2 - Wrapping(10);
+    let tiel2 = tiel1 + Wrapping(7);
+    let result = tiel2 - Wrapping(3);
+    assert_eq!(result.0, 0);
 }
 
 #[test]
@@ -64,14 +67,16 @@ fn three_ints() {
     let func = module.get_func_by_name("three_ints").expect("Failed to find function");
     let args = find_zero_of_func(func, 20).expect("Failed to find zero of function");
     assert_eq!(args.len(), 2);
-    let x = args[0].unwrap_to_i32();
-    let y = args[1].unwrap_to_i32();
+    let x = Wrapping(args[0].unwrap_to_i32());
+    let y = Wrapping(args[1].unwrap_to_i32());
+    println!("x = {}, y = {}", x, y);
     let tiel1 = x + y;
     let tiel2 = x - y;
     let tiel3 = tiel1 + tiel2;
-    let _tiel2 = tiel3 - 2 * tiel1;
+    let _tiel2 = tiel3 - Wrapping(2) * tiel1;
     let tiel1 = tiel3 - x;
-    assert_eq!(tiel1 - 3, 0);
+    let result = tiel1 - Wrapping(3);
+    assert_eq!(result.0, 0);
 }
 
 #[test]
@@ -81,17 +86,19 @@ fn mismatched() {
     let func = module.get_func_by_name("mismatched").expect("Failed to find function");
     let args = find_zero_of_func(func, 20).expect("Failed to find zero of function");
     assert_eq!(args.len(), 2);
-    let x = args[0].unwrap_to_i8();
-    let y = args[1].unwrap_to_i32();
-    let mmel1 = x + 3;
-    let mmel2 = y - 3;
+    let x = Wrapping(args[0].unwrap_to_i8());
+    let y = Wrapping(args[1].unwrap_to_i32());
+    println!("x = {}, y = {}", x, y);
+    let mmel1 = x + Wrapping(3);
+    let mmel2 = y - Wrapping(3);
     let mmel3 = mmel1 - x;
-    let _mmel1 = (mmel2 - i32::from(mmel3)) as i8;
-    let mmel2 = mmel3 + 4;
+    let _mmel1 = Wrapping((mmel2 - Wrapping(i32::from(mmel3.0))).0 as i8);
+    let mmel2 = mmel3 + Wrapping(4);
     let mmel1 = mmel2 - x;
-    let _mmel3 = mmel2 - 5;
-    let mmel2 = i32::from(mmel1) + y;
-    assert_eq!(mmel2 + 3 * i32::from(x), 0);
+    let _mmel3 = mmel2 - Wrapping(5);
+    let mmel2 = Wrapping(i32::from(mmel1.0)) + y;
+    let result = mmel2 + Wrapping(3) * Wrapping(i32::from(x.0));
+    assert_eq!(result.0, 0);
 }
 
 #[test]
@@ -101,16 +108,18 @@ fn nested() {
     let func = module.get_func_by_name("nested").expect("Failed to find function");
     let args = find_zero_of_func(func, 20).expect("Failed to find zero of function");
     assert_eq!(args.len(), 2);
-    let x = args[0].unwrap_to_i8();
-    let y = args[1].unwrap_to_i32();
-    let nmmel2 = 0;
-    let _ntiel2 = y + 3;
-    let nmmel1 = x - 4;
+    let x = Wrapping(args[0].unwrap_to_i8());
+    let y = Wrapping(args[1].unwrap_to_i32());
+    println!("x = {}, y = {}", x, y);
+    let nmmel2 = Wrapping(0);
+    let _ntiel2 = y + Wrapping(3);
+    let nmmel1 = x - Wrapping(4);
     let ntiel1 = nmmel2 + y;
-    let nmmel3 = nmmel1 + 10;
+    let nmmel3 = nmmel1 + Wrapping(10);
     let _nmmel2 = nmmel3 + nmmel1;
-    let ntiel2 = i32::from(nmmel3) + ntiel1;
-    assert_eq!(ntiel2 - y, 0);
+    let ntiel2 = Wrapping(i32::from(nmmel3.0)) + ntiel1;
+    let result = ntiel2 - y;
+    assert_eq!(result.0, 0);
 }
 
 #[test]
@@ -120,12 +129,14 @@ fn with_array() {
     let func = module.get_func_by_name("with_array").expect("Failed to find function");
     let args = find_zero_of_func(func, 20).expect("Failed to find zero of function");
     assert_eq!(args.len(), 1);
-    let x = args[0].unwrap_to_i32();
-    let waarr2 = x + 4;
-    let waarr4 = -3;
+    let x = Wrapping(args[0].unwrap_to_i32());
+    println!("x = {}", x);
+    let waarr2 = x + Wrapping(4);
+    let waarr4 = Wrapping(-3);
     let _wammel2 = waarr2;
     let wamm2el2 = waarr2 + x;
-    assert_eq!(waarr4 - wamm2el2, 0);
+    let result = waarr4 - wamm2el2;
+    assert_eq!(result.0, 0);
 }
 
 #[test]
@@ -134,12 +145,14 @@ fn structptr() {
     let module = get_module();
     let func = module.get_func_by_name("structptr").expect("Failed to find function");
     let args = find_zero_of_func(func, 20).expect("Failed to find zero of function");
-    assert_eq!(args.len(), 2);
-    let x = args[0].unwrap_to_i32();
-    let mmel3 = 0;
-    let mmel2 = x + 4;
+    assert_eq!(args.len(), 1);
+    let x = Wrapping(args[0].unwrap_to_i32());
+    println!("x = {}", x);
+    let mmel3 = Wrapping(0);
+    let mmel2 = x + Wrapping(4);
     let mmel1 = mmel3 + x;
-    assert_eq!(mmel2 + mmel1, 0);
+    let result = mmel2 + mmel1;
+    assert_eq!(result.0, 0);
 }
 
 #[test]
@@ -148,16 +161,18 @@ fn ptrs() {
     let module = get_module();
     let func = module.get_func_by_name("ptrs").expect("Failed to find function");
     let args = find_zero_of_func(func, 20).expect("Failed to find zero of function");
-    assert_eq!(args.len(), 2);
-    let x = args[0].unwrap_to_i32();
-    let wa1mmel2 = 0;
-    let wa2arr7 = 0;
-    let wa1arr3 = x + 4;
-    let wa2arr4 = x + 7;
-    let wa2mm2el2 = wa1mmel2 + 3;
+    assert_eq!(args.len(), 1);
+    let x = Wrapping(args[0].unwrap_to_i32());
+    println!("x = {}", x);
+    let wa1mmel2 = Wrapping(0);
+    let wa2arr7 = Wrapping(0);
+    let wa1arr3 = x + Wrapping(4);
+    let wa2arr4 = x + Wrapping(7);
+    let wa2mm2el2 = wa1mmel2 + Wrapping(3);
     let _wa1arr7 = wa2arr4 + wa1arr3;
     let wa2arr1 = wa2arr7 - wa2mm2el2;
     let wa1arr5 = wa1mmel2 + wa1arr3;
-    let wa2mmel2 = wa2mm2el2 + 3;
-    assert_eq!(wa2mmel2 + wa2arr1 + wa1arr5 + wa1arr5, 0);
+    let wa2mmel2 = wa2mm2el2 + Wrapping(3);
+    let result = wa2mmel2 + wa2arr1 + wa1arr5 + wa1arr5;
+    assert_eq!(result.0, 0);
 }
