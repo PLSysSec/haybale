ebed19c46ac5ebd1db96efe420df12fe043e171a
Craig Disselkoen <craigdissel@gmail.com>
2019-07-22 21:20:01 -0700

    add some new tests (currently failing)

---
+++ tests/linkedlist_tests.rs
@@ -0,0 +1,34 @@
+use llvm_ir::Module;
+use pitchfork_rs::*;
+use std::path::Path;
+
+fn init_logging() {
+    // capture log messages with test harness
+    let _ = env_logger::builder().is_test(true).try_init();
+}
+
+fn get_module() -> Module {
+    Module::from_bc_path(&Path::new("tests/bcfiles/linkedlist.bc"))
+        .expect("Failed to parse module")
+}
+
+#[test]
+fn simple_linked_list() {
+    init_logging();
+    let module = get_module();
+    let func = module.get_func_by_name("simple_linked_list").expect("Failed to find function");
+    let args = find_zero_of_func(func, 20).expect("Failed to find zero of function");
+    assert_eq!(args.len(), 1);
+    assert_eq!(args[0], SolutionValue::I32(3));
+}
+
+
+#[test]
+fn indirectly_recursive_type() {
+    init_logging();
+    let module = get_module();
+    let func = module.get_func_by_name("indirectly_recursive_type").expect("Failed to find function");
+    let args = find_zero_of_func(func, 20).expect("Failed to find zero of function");
+    assert_eq!(args.len(), 1);
+    assert_eq!(args[0], SolutionValue::I32(3));
+}
