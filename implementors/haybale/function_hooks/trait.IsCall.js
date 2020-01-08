ecfb33dfa86aa921a487f24b691842121be585fd
Craig Disselkoen <craigdissel@gmail.com>
2020-01-08 15:58:07 -0800

    Update documentation

---
+++ implementors/haybale/function_hooks/trait.IsCall.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["haybale"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["haybale"] = {"name":"","files":["alloc.rs","alloc_utils.rs","backend.rs","config.rs","default_hooks.rs","double_keyed_map.rs","error.rs","extend.rs","global_allocations.rs","layout.rs","lib.rs","memory.rs","project.rs","return_value.rs","simple_memory.rs","solver_utils.rs","state.rs","symex.rs","varmap.rs","watchpoints.rs"]};
+sourcesIndex["haybale"] = {"name":"","dirs":[{"name":"hooks","files":["allocation.rs","exceptions.rs","intrinsics.rs"]}],"files":["alloc.rs","alloc_utils.rs","backend.rs","config.rs","double_keyed_map.rs","error.rs","extend.rs","function_hooks.rs","global_allocations.rs","hooks.rs","layout.rs","lib.rs","memory.rs","project.rs","return_value.rs","simple_memory.rs","solver_utils.rs","state.rs","symex.rs","varmap.rs","watchpoints.rs"]};
 createSourceSidebar();
