2d9c8dfb56520c9ef1842b2b9b6c337afeee731a
Craig Disselkoen <craigdissel@gmail.com>
2020-01-15 16:27:28 -0800

    Update documentation

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["haybale"] = {"name":"","dirs":[{"name":"hooks","files":["allocation.rs","exceptions.rs","intrinsics.rs"]}],"files":["alloc.rs","alloc_utils.rs","backend.rs","config.rs","double_keyed_map.rs","error.rs","extend.rs","function_hooks.rs","global_allocations.rs","hooks.rs","layout.rs","lib.rs","memory.rs","project.rs","return_value.rs","simple_memory.rs","solver_utils.rs","state.rs","symex.rs","varmap.rs","watchpoints.rs"]};
+sourcesIndex["haybale"] = {"name":"","dirs":[{"name":"hooks","files":["allocation.rs","exceptions.rs","intrinsics.rs"]}],"files":["alloc.rs","alloc_utils.rs","backend.rs","config.rs","demangling.rs","double_keyed_map.rs","error.rs","extend.rs","function_hooks.rs","global_allocations.rs","hooks.rs","layout.rs","lib.rs","memory.rs","project.rs","return_value.rs","simple_memory.rs","solver_utils.rs","state.rs","symex.rs","varmap.rs","watchpoints.rs"]};
 createSourceSidebar();
