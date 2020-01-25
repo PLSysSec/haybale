c00f9bac9032235cea5fe3791c2cda3c1ecb38cf
Craig Disselkoen <craigdissel@gmail.com>
2020-01-25 12:10:34 -0800

    remove BoolCoercionError
    hasn't been used in a long time - I think since we switched to
    Boolector?

--- src/error.rs
+++ src/error.rs
@@ -17,8 +17,6 @@ pub enum Error {
     NullPointerDereference,
     /// Processing a call of a function with the given name, but failed to find an LLVM definition, a function hook, or a built-in handler for it
     FunctionNotFound(String),
-    /// An operation attempted to coerce a `BV` more than one bit long into a `Bool`. The `String` is a text description of the `BV`, and the `u32` is its size
-    BoolCoercionError(String, u32),
     /// The solver returned this processing error while evaluating a query.
     /// Often, this is a timeout; see [`Config.solver_query_timeout`](config/struct.Config.html#structfield.solver_query_timeout)
     SolverError(String),
@@ -41,8 +39,6 @@ impl fmt::Display for Error {
                 write!(f, "`NullPointerDereference`: the current path has attempted to dereference a null pointer"),
             Error::FunctionNotFound(funcname) =>
                 write!(f, "`FunctionNotFound`: encountered a call of a function named {:?}, but failed to find an LLVM definition, a function hook, or a built-in handler for it", funcname),
-            Error::BoolCoercionError(bv, size) =>
-                write!(f, "`BoolCoercionError`: can't coerce a BV {} bits long into a Bool; the BV was {}", size, bv),
             Error::SolverError(details) =>
                 write!(f, "`SolverError`: the solver returned this error while evaluating a query: {}", details),
             Error::UnsupportedInstruction(details) =>
