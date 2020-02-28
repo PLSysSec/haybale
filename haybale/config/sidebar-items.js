b43152baf0e19ff19d51e6fc59dc8df9d6188707
Craig Disselkoen <craigdissel@gmail.com>
2020-02-28 10:15:16 -0800

    Update documentation

--- haybale/config/sidebar-items.js
+++ haybale/config/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Concretize",""],["Demangling",""]],"struct":[["Config","Various settings which affect how the symbolic execution is performed."]]});
+initSidebarItems({"enum":[["Concretize","Enum used for the `concretize_memcpy_lengths` option in `Config`."],["Demangling","Enum used for the `demangling` option in `Config`."]],"struct":[["Config","Various settings which affect how the symbolic execution is performed."]]});
