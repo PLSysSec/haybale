24dd2a6d8c8fb21a52160ae7cfb4ea81822f3cab
Craig Disselkoen <craigdissel@gmail.com>
2020-03-31 12:00:07 -0700

    Update documentation

--- implementors/core/iter/traits/collect/trait.FromIterator.js
+++ implementors/core/iter/traits/collect/trait.FromIterator.js
@@ -1,9 +1,3 @@
 (function() {var implementors = {};
-implementors["haybale"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.FromIterator.html\" title=\"trait core::iter::traits::collect::FromIterator\">FromIterator</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.tuple.html\">(</a><a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/string/struct.String.html\" title=\"struct alloc::string::String\">String</a>, <a class=\"struct\" href=\"haybale/watchpoints/struct.Watchpoint.html\" title=\"struct haybale::watchpoints::Watchpoint\">Watchpoint</a><a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.tuple.html\">)</a>&gt; for <a class=\"struct\" href=\"haybale/watchpoints/struct.Watchpoints.html\" title=\"struct haybale::watchpoints::Watchpoints\">Watchpoints</a>",synthetic:false,types:["haybale::watchpoints::Watchpoints"]},];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+implementors["haybale"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.FromIterator.html\" title=\"trait core::iter::traits::collect::FromIterator\">FromIterator</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.tuple.html\">(</a><a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/string/struct.String.html\" title=\"struct alloc::string::String\">String</a>, <a class=\"struct\" href=\"haybale/watchpoints/struct.Watchpoint.html\" title=\"struct haybale::watchpoints::Watchpoint\">Watchpoint</a><a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.tuple.html\">)</a>&gt; for <a class=\"struct\" href=\"haybale/watchpoints/struct.Watchpoints.html\" title=\"struct haybale::watchpoints::Watchpoints\">Watchpoints</a>","synthetic":false,"types":["haybale::watchpoints::Watchpoints"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
