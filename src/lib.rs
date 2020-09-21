5cacd666916ecaa8797a54ee5ee44ce04a2a7c6c
Craig Disselkoen <craigdissel@gmail.com>
2020-09-21 11:42:52 -0700

    use zip_eq in a few more places

--- src/lib.rs
+++ src/lib.rs
@@ -47,6 +47,7 @@ mod varmap;
 pub mod watchpoints;
 
 use backend::*;
+use itertools::Itertools;
 use solver_utils::PossibleSolutions;
 
 #[cfg(test)]
@@ -170,7 +171,7 @@ pub fn find_zero_of_func<'p>(
         Ok(Some(
             func.parameters
                 .iter()
-                .zip(param_bvs.iter())
+                .zip_eq(param_bvs.iter())
                 .map(|(p, bv)| {
                     let param_as_u64 = state
                         .get_a_solution_for_bv(bv)?
