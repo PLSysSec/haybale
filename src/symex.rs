36ffa32043128e95e58a62a18bbcb316e53c2005
Hudson Ayers <32688905+hudson-ayers@users.noreply.github.com>
2022-05-10 17:31:15 -0700

    ignore noalias intrinsic (#23)

--- src/symex.rs
+++ src/symex.rs
@@ -1723,6 +1723,7 @@ where
                         || funcname.starts_with("llvm.strip.invariant")
                         || funcname.starts_with("llvm.dbg")
                         || funcname.starts_with("llvm.expect")
+                        || funcname.starts_with("llvm.experimental.noalias.scope.decl")
                     {
                         // these are all safe to ignore
                         Ok(ResolvedFunction::HookActive {
